b0030992b48ba95592b1badc761ecbe3a3bc2371
Anne van Kesteren <annevk@opera.com>
2012-03-05 21:57:03 +0100

    lots of new HTML for html5-diff; more to come

--- xrefs/dom/html.py
+++ xrefs/dom/html.py
@@ -12,8 +12,15 @@ localData = json.loads(open("html.json", "r").read())
 
 # Updates our local mapping of terms and identifiers with links to the
 # multipage version. This will fail if our local mapping is wrong.
+errors = []
 for term, identifier in localData["definitions"].items():
+    if identifier not in multipageData:
+        errors.append(identifier)
+        continue
     localData["definitions"][term] = multipageData[identifier] + ".html#" + identifier
+
+if errors:
+    raise Exception, errors
 
 handle = open("html-generated.json", "w")
 handle.write(json.dumps(localData, sort_keys=True, allow_nan=False, indent=2, separators=(',', ': ')))
